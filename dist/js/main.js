1c276f5cabc31199443107a543c3f6688550dab9
Martin Oshaughnessy <vsredshift@gmail.com>
2021-03-31 08:59:09 +0200

    refactor code

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+const menuPic = document.querySelector('.menu-pic');
 const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State of Menu
@@ -16,7 +16,7 @@ function toggleMenu() {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+        menuPic.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
         // Set Menu State
@@ -25,7 +25,7 @@ function toggleMenu() {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
+        menuPic.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
         showMenu = false;
