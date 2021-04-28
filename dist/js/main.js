52b7bb0cbbf60ceff3b06ac724f57308cfb2e842
Martin Oshaughnessy <vsredshift@gmail.com>
2021-04-28 22:26:13 +0200

    Add project thumbnails

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,51 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+// Change project thumbnails on hover
+const frontendImage = document.getElementById("frontend").getElementsByTagName("img")[0];
+const images = ["../images/gepetto.png", "../images/gmusic.png", "../images/getaway.png", "../images/business.png", "../images/glabws.png"];
+
+const coronaImage = document.getElementById("corona").getElementsByTagName("img")[0];
+const coronaPics = ["../images/coronaRolling.png", "../images/coronaTable.png", "../images/coronaComp.png"];
+
+
+var timer;
+var i = 1;
+
+const change = () => {
+    frontendImage.src = images[i];
+    if (++i >= images.length) {
+        i = 0
+    }
+    timer= setTimeout(change, 1200);
+};
+
+frontendImage.onmouseover = () => {
+    change();
+};
+
+frontendImage.onmouseout = () => {
+    frontendImage.src = "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+    clearTimeout(timer);
+};
+
+// Corona
+var k = 1;
+const chuck = function () {
+    coronaImage.src = coronaPics[k];
+    if (++k >= coronaPics.length) {
+        k = 0
+    }
+    timer= setTimeout(chuck, 1200);
+};
+
+coronaImage.onmouseover = () => {
+    chuck();
+};
+
+coronaImage.onmouseout = () => {
+    coronaImage.src = "https://images.pexels.com/photos/159888/pexels-photo-159888.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+    clearTimeout(timer);
+};
+
